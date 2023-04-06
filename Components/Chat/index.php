104bccf5f9640f0a7d30ed5f676ab28dca72fbcc
zertidana <94858570+zertidana@users.noreply.github.com>
2023-04-06 18:58:58 +0100

    i

--- Components/Chat/index.php
+++ Components/Chat/index.php
@@ -1,4 +1,31 @@
 <! DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset = "utf-8"
+        <meta charset = "utf-8" />
+
+        <title> Chat </title>
+        <meta name = "desc" content = "Chat" />
+        <link rel = "stylesheet" href = "chat.css" />
+    </head>
+    <body>
+        <div id = "wrapper">
+            <div id = "menu">
+                <p></p>
+                <p><a id = "exit" href = "#"> Exit Chat </a></p>
+            </div>
+
+            <div id = "chatbox"></div>
+
+            <form name = "message" action = "">
+                <input name = "user" type = "text" id = "user" />
+                <input name = "submit" value = "send" type = "submit" id = "submit" />
+            </form>
+        </div>
+        <script type = "test/javascript" src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"> </script>
+        <script type = "text/javascript">
+            $(Document).ready(function () {});
+        </script>
+    </body>
+</html>
+
+
