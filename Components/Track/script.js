666be910daceb7475868b20292b5f6b4fb514df3
FDan01 <95966073+FDan01@users.noreply.github.com>
2023-04-09 13:55:11 +0100

    Update script.js

--- Components/Track/script.js
+++ Components/Track/script.js
@@ -1,6 +1,10 @@
 function addMeal() {
   var foodName = document.getElementById("foodName").value;
   var calories = parseInt(document.getElementById("calories").value);
+  if (isNaN(calories) || calories < 0) {
+    alert("Please enter a valid number of calories.");
+    return;
+  }
   document.getElementById("caloriesConsumed").innerHTML = parseInt(document.getElementById("caloriesConsumed").innerHTML) + calories;
   document.getElementById("netCalories").innerHTML = parseInt(document.getElementById("caloriesConsumed").innerHTML) - parseInt(document.getElementById("caloriesBurned").innerHTML);
 }
@@ -8,7 +12,28 @@ function addMeal() {
 function trackExercise() {
   var exerciseType = document.getElementById("exerciseType").value;
   var duration = parseInt(document.getElementById("duration").value);
-  var caloriesBurned = duration * 7; // Assuming 7 calories burned per minute at moderate intensity
+  var intensity = document.getElementById("intensity").value;
+  var caloriesPerMinute;
+
+  switch (intensity) {
+    case "low":
+      caloriesPerMinute = 5;
+      break;
+    case "moderate":
+      caloriesPerMinute = 7;
+      break;
+    case "high":
+      caloriesPerMinute = 9;
+      break;
+    default:
+      caloriesPerMinute = 0;
+  }
+
+  var caloriesBurned = duration * caloriesPerMinute;
+  if (isNaN(caloriesBurned) || caloriesBurned < 0) {
+    alert("Please enter a valid duration and intensity.");
+    return;
+  }
   document.getElementById("caloriesBurned").innerHTML = parseInt(document.getElementById("caloriesBurned").innerHTML) + caloriesBurned;
   document.getElementById("netCalories").innerHTML = parseInt(document.getElementById("caloriesConsumed").innerHTML) - parseInt(document.getElementById("caloriesBurned").innerHTML);
 }
@@ -17,9 +42,14 @@ document.getElementById("calculateBtn").addEventListener("click", calculateCalor
 
 function calculateCalories() {
   const gender = document.getElementById("gender").value;
-  const age = document.getElementById("age").value;
-  const height = document.getElementById("height").value;
-  const weight = document.getElementById("weight").value;
+  const age = parseInt(document.getElementById("age").value);
+  const height = parseInt(document.getElementById("height").value);
+  const weight = parseInt(document.getElementById("weight").value);
+
+  if (isNaN(age) || isNaN(height) || isNaN(weight) || age <= 0 || height <= 0 || weight <= 0) {
+    alert("Please enter a valid age, height, and weight.");
+    return;
+  }
 
   let calories;
   if (gender === "male") {
@@ -28,21 +58,20 @@ function calculateCalories() {
     calories = (447.6 + 9.25 * weight) + (3.10 * height) - (4.33 * age);
   }
 
-  const goal = prompt("What is your goal? (lose/maintain/gain)");
-    let message;
-    if (goal === "lose") {
-      message = "You should eat 200 calories less than your maintenance value.";
-    } else if (goal === "maintain") {
-      message = "You should eat your maintenance value.";
-    } else if (goal === "gain") {
-      message = "You should eat 200 calories above your maintenance value.";
-    } else {
-      message = "Invalid goal.";
-    }
+  calories *= 1.375; 
+  // assuming moderate activity saf which is multiplied with bmr will be 1.375
 
-    document.getElementById("result").innerHTML = "Your maintenance daily calorie intake is: " + calories.toFixed(2) + " calories. " + message;
+  const goal = document.getElementById("goal").value;
+  let message;
+  if (goal === "lose") {
+    message = "You should eat 200 calories less than your maintenance value.";
+  } else if (goal === "maintain") {
+    message = "You should eat your maintenance value.";
+  } else if (goal === "gain") {
+    message = "You should eat 200 calories above your maintenance value.";
+  } else {
+    message = "Invalid goal.";
+  }
 
-  document.getElementById("calculateBtn").addEventListener("click", calculateCalories);
-
-
+  document.getElementById("result").innerHTML = "Your maintenance daily calorie intake is: " + calories.toFixed(2) + " calories. " + message;
 }
