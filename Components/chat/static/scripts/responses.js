ae3eea3bc69c832b3e4401ebc80571371f2c15b4
zertidana <94858570+zertidana@users.noreply.github.com>
2023-04-11 16:50:25 +0100

    chat

---
+++ Components/chat/static/scripts/responses.js
@@ -0,0 +1,17 @@
+function getBotResponse(input) {
+    if (input.includes("happy")) {
+        return "That's great!";
+    } else if (input.includes("sad")) {
+        return "What made you upset?";
+    } else if (input.includes("excited")) {
+        return "Yay! Tell me more!";
+    } else if (input.includes("hello")) {
+        return "Hello there! How are you doing?";
+    } else if (input.includes("goodbye")) {
+        return "Talk to you later!";
+    } else if (input.includes("bye")) {
+        return "Talk to you later!";
+    } else {
+        return "Can you re-word that?";
+    }
+}
