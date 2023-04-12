3e2287ec216b0860323e61e748cfc33346e981e1
FDan01 <95966073+FDan01@users.noreply.github.com>
2023-04-12 22:34:09 +0100

    Add files via upload

---
+++ Components/footer/feedback/feedback.js
@@ -0,0 +1,7 @@
+
+document.querySelector('.feedbackform').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+  
+    
+    window.location.href = '../../homepage/home.html';
+  });
