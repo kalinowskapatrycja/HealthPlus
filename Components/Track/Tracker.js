592df673edc5e4cf7fb1cc2f44cb3914c730ba81
Bekim Gerguri <76046983+BekimG@users.noreply.github.com>
2023-04-12 13:52:38 +0100

    Update Tracker.js

---
+++ Components/Track/Tracker.js
@@ -0,0 +1,14 @@
+const moodForm = document.getElementById('mood-tracking');
+
+moodForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const mentalHealth = document.getElementById('mental-health').value;
+    const stressLevel = document.getElementById('stress-level').value;
+    const sleepQuality = document.getElementById('sleep-quality').value;
+
+    sessionStorage.setItem('currentMood', mentalHealth);
+    sessionStorage.setItem('stressLevel', stressLevel);
+    sessionStorage.setItem('sleepQuality', sleepQuality);
+
+    moodForm.submit();
+});
